09e13ada94c45e6c921fd2c057c24e7e0fc52821
lazarevaak (Лазарева Александра) <lazarevaak@yandex.ru>
2025-11-14 22:36:57 +0300

    homework7

--- app/database.py
+++ app/database.py
@@ -1,21 +1,21 @@
-from __future__ import annotations
-
+import os
 from typing import Generator
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
 
-DATABASE_URL = "sqlite:///./studyplan.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./studyplan.db")
 
 
 class Base(DeclarativeBase):
     pass
 
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(
-    bind=engine, autoflush=False, autocommit=False, class_=Session
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False},
 )
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 
 def get_db() -> Generator[Session, None, None]:
