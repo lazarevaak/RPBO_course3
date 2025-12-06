3eec07f5da66256ab770f9a96d5f2d9e03a8d608
lazarevaak (Лазарева Александра) <lazarevaak@yandex.ru>
2025-12-07 02:19:35 +0300

    с4

--- app/database.py
+++ app/database.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from pathlib import Path
 from typing import Generator
@@ -5,26 +6,26 @@ from typing import Generator
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
 
+logger = logging.getLogger("database")
+logger.setLevel(logging.INFO)
+
 env = os.getenv("ENV", "local").lower()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-
 if DATABASE_URL:
-    print(f"[INFO] Using DATABASE_URL from environment: {DATABASE_URL}")
+    logger.info("Using DATABASE_URL from environment: %s", DATABASE_URL)
 
 else:
     if env == "ci":
         sqlite_path = Path("./ci-test.db")
-
     elif env == "prod":
         sqlite_path = Path("/var/data/studyplan.db")
-
     else:
         sqlite_path = Path("./studyplan.db")
 
     sqlite_path.parent.mkdir(parents=True, exist_ok=True)
     DATABASE_URL = f"sqlite:///{sqlite_path}"
-    print(f"[INFO] {env.upper()} mode: using SQLite at {sqlite_path}")
+    logger.info("%s mode: using SQLite at %s", env.upper(), sqlite_path)
 
 
 class Base(DeclarativeBase):
